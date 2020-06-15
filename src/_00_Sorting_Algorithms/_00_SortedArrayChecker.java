49e6ba6368c0e67a9b354ac3e61961e26db97765
Athena Hernández <64385980+athenachernandez@users.noreply.github.com>
2020-06-15 13:29:36 -0700

    sorting

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -6,13 +6,29 @@ public class _00_SortedArrayChecker {
 	//   and it returns a boolean.
 	//   The method returns true if the integer
 	//   array is in ascending order and false otherwise
-
+	public static boolean intArraySorted(int[] array) {
+		for (int i = 0; i < array.length-1; i++) {
+			if (array[i] > array[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	
 	//2. Write a static method called doubleArraySorted. 
 	//   This method takes in an array of doubles
 	//   and it returns a boolean.
 	//   The method returns true if the double
 	//   array is in ascending order and false otherwise
+	public static boolean doubleArraySorted(double[] array) {
+		for (int i = 0; i < array.length-1; i++) {
+			if (array[i] > array[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 	
 	//3. Write a static method called charArraySorted. 
@@ -21,6 +37,14 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the character
 	//   array is in alphabetical order and false otherwise
 	//   (You can compare characters just like integers)
+	public static boolean charArraySorted(char[] array) {
+		for (int i = 0; i < array.length-1; i++) {
+			if (array[i] > array[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 	
 	//4. Write a static method called stringArraySorted. 
@@ -29,5 +53,15 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the String
 	//   array is in alphabetical order and false otherwise
 	//   (Use the compareTo(String) method)
+	public static boolean stringArraySorted(String[] array) {
+		for (int i = 0; i < array.length-1; i++) {
+			String string1 = array[i];
+			String string2 = array[i+1];
+			if (string1.compareTo(string2) > 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
